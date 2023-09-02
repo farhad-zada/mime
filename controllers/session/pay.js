879c1cd8b47c4efc82d085cf5088d049dab43499
Farhad <farhad.szd@gmail.com>
2023-09-02 14:53:41 +0400

    Payment task on development

--- controllers/session/pay.js
+++ controllers/session/pay.js
@@ -8,6 +8,9 @@ const AppError = require('../../utils/appError')
 const catchAsync = require('../../utils/catchAsync')
 
 module.exports = catchAsync(async (req, res, next) => {
+  //TODO: put your code here. And if needed to another file please contact me on whatsapp and let me know it too.
+  // Good luck Elmir!
+
   return next(
     new AppError(
       'This route not been implemented yet',
